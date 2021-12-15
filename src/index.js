1fdede9ee0a8284ef58423a97349a49101d4f205
GratzJulia <gratzjulia@gmail.com>
2021-12-14 23:17:19 -0200

    feat: componentizando tela EdicaoAtividade

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Footer from './components/Footer/index'
 import App from './App';
+import EdicaoAtividade from './components/EdicaoAtividade';
 import PainelAtividade from './components/PainelAtividade/index'
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 ReactDOM.render(
   <React.StrictMode>
-    <PainelAtividade />
+    <EdicaoAtividade />
     <Footer />
   </React.StrictMode>,
   document.getElementById('root')
