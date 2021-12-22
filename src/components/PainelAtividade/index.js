505a6569f8e729903784f1301828bf095aa2c7c2
Julia Gratz <julia.gratz@aluno.cefet-rj.br>
2021-12-21 22:26:45 -0300

    Ajustes

--- src/components/PainelAtividade/index.js
+++ src/components/PainelAtividade/index.js
@@ -23,12 +23,10 @@ function PainelAtividade(props) {
   }
 
   return(
-    <>
     <div className='wrapper'>
       <div className='accordion'>
-        {cards.map((item, i) => (
-          <>
-          <Card style={{ marginBottom: '1rem' }}>
+        {cards.map((item, i) => (          
+          <Card style={{ marginBottom: '1rem' }} key={i}>
             <CardHeader className={selected === i ? 'accordion-button' : 'accordion-button collapsed'} onClick={() => toggle(i)}>{item.name}</CardHeader>
             <Collapse className={selected === i ? 'content show' : 'content'}>
               <CardBody>
@@ -42,32 +40,27 @@ function PainelAtividade(props) {
               </CardBody>
               <CardBody className={items != null ? 'pad' : ''}>
                 {isNewActivity === i ? 
-                  <AdicionarAtividade onClose={() => setIsNewActivity(false)} activities={props.activities} setActivities={props.setActivities} /> 
+                  <AdicionarAtividade onClose={() => setIsNewActivity(false)} activities={props.activities} setActivities={props.setActivities} card={item} /> 
                 : null}
               </CardBody>
               <hr />
               <ActivityList activities={props.activities} />
             </Collapse>
           </Card>
-          </> 
         ))}
       </div>
     </div>
-    </>
   );
 }
 
 const ActivityLine = (props) => {
-  return (
-    <>
-    <div class="activity_list container row">
-      <div class="col-4">{props.activity.type}</div>
-      <div class="col-4">{props.activity.description}</div>
-      <div class="col-2">{props.activity.hours}</div>
-      <div class="col-2">{props.activity.attachment}</div>
-    </div>
-      
-    </>
+  return (    
+    <div className='activity_list container row'>
+      <div className='col-4'>{props.activity.type}</div>
+      <div className='col-4'>{props.activity.description}</div>
+      <div className='col-2'>{props.activity.hours}</div>
+      <div className='col-2'>{props.activity.attachment}</div>
+    </div>    
   );
 }
 
@@ -81,18 +74,33 @@ const cards = [
   {
     id: 1, 
     name: 'Pesquisa',
-  }, 
+    subcategories: [{id: 1, name: 'Iniciação científica'},
+                    {id: 2, name: 'Publicações'},
+                    {id: 3, name: 'Participação em projetos de pesquisa'},
+                    {id: 4, name: 'Assistência a monografias, teses e dissertações'}]
+  },
   {
     id: 2, 
     name: 'Extensão',
-  }, 
+    subcategories: [{id: 1, name: 'Organização e/ou colaboração em eventos e atividades institucionais'},
+                    {id: 2, name: 'Seminários, conferências, palestras, oficinas e visitas técnicas'},
+                    {id: 3, name: 'Participação em projetos de extensão'},
+                    {id: 4, name: 'Presença em bancas de projeto final de curso'},
+                    {id: 5, name: 'Cursos de atualização, qualificação e certificação tecnológica'},
+                    {id: 6, name: 'Cursos de língua estrangeira'},
+                    {id: 7, name: 'Assistência, assessoria e consultoria técnica'}]
+  },
   {
     id: 3, 
     name: 'Ensino',
-  }, 
+    subcategories: [{id: 1, name: 'Disciplinas não previstas'},
+                    {id: 2, name: 'Monitoria'}]
+  },
   {
     id: 4, 
     name: 'AC HC ou Ambiental',
+    subcategories: [{id: 1, name: 'Conscientização de questões histórico-culturais'},
+                    {id: 2, name: 'Ambientais'}]
   }
 ]
 
