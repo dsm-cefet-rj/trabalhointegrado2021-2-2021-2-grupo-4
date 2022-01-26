6eb91ee627d68bc40fb4123c2d99295eea23dcc2
Jorge Gabriel Rodrigues <jorgegabriel99h@gmail.com>
2022-01-26 16:19:39 -0300

    feat: testing delete and get with backend express

--- backend/sghc/routes/activities.js
+++ backend/sghc/routes/activities.js
@@ -1,20 +1,45 @@
 var express = require('express');
 var router = express.Router();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+
+let activities = [
+  {
+    "type": "Iniciação científica",
+    "description": "sfsaf",
+    "hours": "20",
+    "id": 1,
+    "category": 1,
+    "validate": false
+  },{
+    "type": "Cursos de língua estrangeira",
+    "description": "espanhol",
+    "hours": "60",
+    "id": 2,
+    "category": 2,
+    "validate": false
+  }
+];
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.route('/')
+.get((req, res, next) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json([
-    {
-      "type": "Iniciação científica",
-      "description": "sfsaf",
-      "hours": "20",
-      "id": 1,
-      "category": 1,
-      "validate": false
-    }
-  ]);
-});
+  res.json(activities);
+})
+
+router.route('/:id')
+.delete((req, res, next) => {
+  activities = activities.filter(function(value, index, arr){
+    return value.id != req.params.id;
+  });
+
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json(req.params.id);
+})
 
 module.exports = router;
