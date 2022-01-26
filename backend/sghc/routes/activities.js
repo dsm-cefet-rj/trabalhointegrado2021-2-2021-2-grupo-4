c7c487e7bd31c0398aa9e8e9c78f96cdae978a36
Jorge Gabriel Rodrigues <jorgegabriel99h@gmail.com>
2022-01-26 18:28:16 -0300

    feat: testing put & post with backend express

--- backend/sghc/routes/activities.js
+++ backend/sghc/routes/activities.js
@@ -29,6 +29,16 @@ router.route('/')
   res.setHeader('Content-Type', 'application/json');
   res.json(activities);
 })
+.post((req, res, next) => {
+  let proxId = 1 + activities.map(p => p.id).reduce((x,y) => Math.max(x,y));
+  let activity = {...req.body, id: proxId }
+  activities.push(activity);
+
+  activities.push(req.body);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json(activity);
+})
 
 router.route('/:id')
 .delete((req, res, next) => {
@@ -36,10 +46,17 @@ router.route('/:id')
     return value.id != req.params.id;
   });
 
-
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json(req.params.id);
+})
+.put((req, res, next) => {
+  let index = activities.map(p => p.id).indexOf(req.params.id);
+  activities.splice(index, 1, req.body);
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json(req.body);
 })
 
 module.exports = router;
