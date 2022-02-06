8171086df456c87e7b6dc366be55a97048803567
Jorge Gabriel Rodrigues <jorgegabriel99h@gmail.com>
2022-02-06 16:10:44 -0300

    feat: update config atlas to sghc

--- backend/sghc/app.js
+++ backend/sghc/app.js
@@ -22,11 +22,11 @@ connect.then((db) => {
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
--- frontend/src/baseUrl.js
+++ frontend/src/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://192.168.0.6:3004';
+export const baseUrl = 'http://localhost:3004';
