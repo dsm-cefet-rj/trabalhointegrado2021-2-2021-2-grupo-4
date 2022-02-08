37006afec60d4af8f8a91c20b46b2792103e61f2
Jorge Gabriel Rodrigues <jorgegabriel99h@gmail.com>
2022-02-08 18:37:46 -0300

    feat: add login with postman without front

--- backend/sghc/app.js
+++ backend/sghc/app.js
@@ -11,6 +11,7 @@ require('dotenv').config();
 var indexRouter = require('./routes/index');
 var activitiesRouter = require('./routes/activities');
 var categoriesRouter = require('./routes/categories');
+var usersRouter = require('./routes/users');
 
 const mongoose = require('mongoose');
 
@@ -37,38 +38,22 @@ app.use(session({
     store: new FileStore()
 }))
 
+//app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next){
     console.log(req.headers);
     if(!req.session.user) {
-        var authHeader = req.headers.authorization;
-        if(!authHeader) {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            next(err);
-            return
-        }
-
-        var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-        var user = auth[0];
-        var pass = auth[1];
-        if(user == 'admin' && pass == 'password') {
-            req.session.user = 'admin';
+        var err = new Error('You are not authenticated!');
+        err.status = 403;
+        return next(err);
+    } else { 
+        if(req.session.user === 'authenticated') {
             next();
         } else {
             var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            next(err);
-        }
-    } else {
-        if (req.session.user === 'admin'){
-            console.log('req.session: ', req.session);
-            next();
-        } else {
-            var err = new Error('You are not authenticated!');
-            err.status = 401;
-            next(err);
+            err.status = 403;
+            return next(err);
         }
     }
 }
@@ -77,7 +62,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/activities', activitiesRouter);
 app.use('/categories', categoriesRouter);
 
